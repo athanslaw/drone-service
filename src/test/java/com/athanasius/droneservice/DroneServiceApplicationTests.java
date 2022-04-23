65ea7603b6b578e21bb88687b4fe43c40bda67d6
Athanasius.Lawrence-dev <Athanasius.Lawrence@ISW-ENG-AL>
2022-04-23 07:46:02 +0100

    Initiated a springboot project with the most basic dependencies. Other dependencies will be added subsequently

---
+++ src/test/java/com/athanasius/droneservice/DroneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athanasius.droneservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
