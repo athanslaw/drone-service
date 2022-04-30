2ba20175975438c6d0b53324e0078f7fa97ad2fc
Athanasius.Lawrence-dev <Athanasius.Lawrence@ISW-ENG-AL>
2022-04-30 17:56:53 +0100

    Trigger / Dispatch drone api

--- src/main/java/com/athanasius/droneservice/controller/DispatchController.java
+++ src/main/java/com/athanasius/droneservice/controller/DispatchController.java
@@ -2,12 +2,14 @@ package com.athanasius.droneservice.controller;
 
 import com.athanasius.droneservice.dto.DispatchDto;
 import com.athanasius.droneservice.exception.BadRequestException;
+import com.athanasius.droneservice.exception.NotFoundException;
 import com.athanasius.droneservice.response.DispatchResponse;
 import com.athanasius.droneservice.services.DispatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,16 @@ public class DispatchController {
       return new ResponseEntity<>(dispatchResponse, HttpStatus.CREATED);
     }catch (BadRequestException e){
       return new ResponseEntity<>(new DispatchResponse(e.getStatusCode(),e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @GetMapping(path= "/trigger/{droneSerialNo}", produces = "application/json")
+  public ResponseEntity<DispatchResponse> setUpDispatch(@PathVariable String droneSerialNo) {
+    try{
+      DispatchResponse dispatchResponse = dispatchService.triggerDispatch(droneSerialNo);
+      return new ResponseEntity<>(dispatchResponse, HttpStatus.CREATED);
+    }catch (NotFoundException ne){
+      return new ResponseEntity<>(new DispatchResponse(ne.getStatusCode(),ne.getMessage()), HttpStatus.NOT_FOUND);
     }
   }
 
